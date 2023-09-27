d717538eff724424fc6eb3a3e38ba2040d2346c8
dmclaughlin104 <dmclaughlin104@gmail.com>
2023-09-27 20:56:07 +0100

    Finished lesson 2.1
    Added player and animals. Created scripts for PlayerController and controlled player boundaries.

---
+++ Unit 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //variables
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float playerXBoundary = 25.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if statement to control left player boundary
+        if (transform.position.x  < -playerXBoundary)
+        {
+            transform.position = new Vector3(-playerXBoundary, transform.position.y, transform.position.z);
+        }//if
+
+        //if statement to control right player boundary
+        if (transform.position.x > playerXBoundary)
+        {
+            transform.position = new Vector3(playerXBoundary, transform.position.y, transform.position.z);
+        }//if
+
+        //assigning horizontal input to correct input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //moves character left/right based on input
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
