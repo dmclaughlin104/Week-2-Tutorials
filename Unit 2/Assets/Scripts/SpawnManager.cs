e6d2c418435c53b84ce45b4df9fe20e86955766c
dmclaughlin104 <dmclaughlin104@gmail.com>
2023-09-29 22:29:43 +0100

    Finished lesson 2.4.3.
    To start at 2.4.4 tomorrow

--- Unit 2/Assets/Scripts/SpawnManager.cs
+++ Unit 2/Assets/Scripts/SpawnManager.cs
@@ -6,27 +6,30 @@ public class SpawnManager : MonoBehaviour
 {
     //variables
     public GameObject[] animalPrefabs;
-    public float topBoundary = 35.0f;
+    private float spawnPositionZ = 30.0f;
+    private float spawnRangeX = 25.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }//start
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.S))
-        {
 
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            float spawnXAxis = Random.Range(-25, 25);
+    }//update
 
-            Instantiate(animalPrefabs[animalIndex], new Vector3(spawnXAxis, 0, 35), animalPrefabs[animalIndex].transform.rotation);
-        }
-        
-    }
+    //method to spawn a random animal in a random location along the 
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+    }//SpawnRandomAnimal
+
 }
